70420c42c5593a510a148bffaf7f62b3c72e4c47
rkdgusdnr99 <112935900+rkdgusdnr99@users.noreply.github.com>
2024-05-06 14:51:33 +0900

    timestamp 사용하기 위해 @EnableJpaAuditing 추가

--- backend/src/main/java/hello/aimju/AiMjuApplication.java
+++ backend/src/main/java/hello/aimju/AiMjuApplication.java
@@ -2,8 +2,10 @@ package hello.aimju;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // timestamp 사용하기 위함
 public class AiMjuApplication {
 
     public static void main(String[] args) {
