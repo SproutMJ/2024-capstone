bc586b11735003c0d173b35aa67a659708547d97
SproutMj <mjk54565@gmail.com>
2024-05-21 14:02:58 +0900

    fix: api 호출 설정 변경

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
     async rewrites() {
         return [
             {
-                source: "/:path*",
-                destination: "http://localhost:8080/:path*",
+                source: "/api/:path*",
+                destination: "http://localhost:8080/api/:path*",
             },
         ];
     },
